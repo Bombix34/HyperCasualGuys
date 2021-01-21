dc9bdba1500dfd6cf02ccd4b8ac6ce7361e78dc6
Nino <ninosciab@gmail.com>
2021-01-21 16:58:05 +0100

    Debut Curseur

---
+++ Assets/Scripts/CursorFeedback.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFeedback : MonoBehaviour
+{
+  
+
+    [SerializeField]
+    private GameObject Cursor;
+
+    private Vector2 MousePosPlan;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePosPlan.x = Input.mousePosition.x;
+            MousePosPlan.y = Input.mousePosition.y;
+            GameObject newCursor = Instantiate(Cursor, MousePosPlan, transform.rotation) as GameObject;
+            newCursor.transform.SetParent(GameObject.FindGameObjectWithTag("Parent").transform, false);
+        }
+    }
+
+}
