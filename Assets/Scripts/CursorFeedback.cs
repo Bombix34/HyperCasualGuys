722d820bece0e7490d2a252d1f3b00dfe332cc48
Nino <ninosciab@gmail.com>
2021-01-22 10:00:17 +0100

    Curseur tentative Destroy

--- Assets/Scripts/CursorFeedback.cs
+++ Assets/Scripts/CursorFeedback.cs
@@ -1,25 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorFeedback : MonoBehaviour
 {
-  
+
 
     [SerializeField]
     private GameObject Cursor;
 
+    [SerializeField]
+    private float fadeDelay = 10f;
+    private float alphaValue = 0;
+    private bool destroyGameObject = false;
+
+
     private Vector2 MousePosPlan;
+
+   
+
+   
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            MousePosPlan.x = Input.mousePosition.x;
-            MousePosPlan.y = Input.mousePosition.y;
-            GameObject newCursor = Instantiate(Cursor, MousePosPlan, transform.rotation) as GameObject;
-            newCursor.transform.SetParent(GameObject.FindGameObjectWithTag("Parent").transform, false);
+            if (destroyGameObject == false)
+            {
+                MousePosPlan.x = Input.mousePosition.x;
+                MousePosPlan.y = Input.mousePosition.y;
+                GameObject newCursor = Instantiate(Cursor, MousePosPlan, transform.rotation) as GameObject;
+                newCursor.transform.SetParent(GameObject.FindGameObjectWithTag("Parent").transform, false);
+                newCursor.transform.position = MousePosPlan;
+                destroyGameObject = true;
+            }
+            else if(destroyGameObject == true)
+            {
+                DestroyCursor();
+            }
+            
+
+            
+
+            
+
+
+
         }
     }
+    private void DestroyCursor()
+    {
+        Destroy(this.gameObject, 2);
+    }
+    
+
 
 }
