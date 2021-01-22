6e5d17aedb910b9378a465f13d26c4b1e11d20d2
Nino <ninosciab@gmail.com>
2021-01-22 15:09:12 +0100

    FeedBack Cursor Fonctionnel

--- Assets/InputIndicator/CursorFeedback.cs
+++ Assets/InputIndicator/CursorFeedback.cs
@@ -6,13 +6,40 @@ using UnityEngine.UI;
 
 public class CursorFeedback : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject feedback;
+
+    [SerializeField]
     private GameObject cursor;
 
-    private Vector2 MousePosPlan;
+    [SerializeField]
+    private GameObject circle;
+
+    [SerializeField]
+    private float fadeInTimeCursor = 0.3f; // Durée Fade In CURSEUR
+    
+    [SerializeField]
+    private float fadeInTimeCircle = 0.3f; // Durée Fade In CERCLE
+
+    [SerializeField]
+    private float fadeOutTimeCursor = 0.2f; // Durée Fade Out CURSEUR
+
+    [SerializeField]
+    private float fadeOutTimeCircle = 0.2f; // Durée Fade Out CERCLE
+
+    [SerializeField]
+    private float tailleMaxVoulueCursor = 1.5f;
+    
+    [SerializeField]
+    private float tailleMaxVoulueCircle = 1.5f;
+
+
 
     private void Start()
     {
-        cursor = GetComponentInChildren<Image>().gameObject;
+        cursor = GetComponent<Image>().gameObject;
+        circle = GetComponent<Image>().gameObject;
+        circle.transform.localScale = Vector3.zero;
         cursor.transform.localScale = Vector3.zero;
     }
 
@@ -20,10 +47,25 @@ public class CursorFeedback : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            cursor.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
-            cursor.transform.DOScale(Vector3.one * 1.5f, 0.3f)
-                .OnComplete(()=> cursor.transform.DOScale(Vector3.zero,0.2f));
+            feedback.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+            CursorFade();
+            CircleFade();
         }
+    }
+
+    
+    private void CursorFade()
+    {
+        
+        cursor.transform.DOScale(Vector3.one * tailleMaxVoulueCursor, fadeInTimeCursor).SetEase(Ease.OutQuint)
+            .OnComplete(() => cursor.transform.DOScale(Vector3.zero, fadeOutTimeCursor)).SetEase(Ease.OutQuint);
+    }
+    
+    private void CircleFade()
+    {
+        
+        circle.transform.DOScale(Vector3.one * tailleMaxVoulueCircle, fadeInTimeCircle).SetEase(Ease.OutQuint)
+            .OnComplete(() => circle.transform.DOScale(Vector3.zero, fadeOutTimeCircle)).SetEase(Ease.OutQuint);
     }
 
 
